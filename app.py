2419026b2bcfbc9e68226aa362dcf4fe55d30cc6
clearr93371-commits <clearr93371@gmail.com>
2026-08-04 10:39:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -400,7 +400,7 @@ def generate_budget(event_type, pax_per_day, days, pax_per_slot, slots, month, e
         if event_type == 'Pop_Up':
             numeric_it_unit_cost = to_num(it['unit_cost'], 0)
             band_fee_est = 3500
-            cost_staff = 8000 * (1.1 ^ ( days - 1 ))
+            cost_staff = 8000 * (1.1 ** ( days - 1 ))
             if any(k in key for k in Fee_CHEFS):
                 qty_to_use = 4
                 days_to_use = days
