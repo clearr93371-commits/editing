5a39abc01cde61fe38c74bfe78474bc0214889b1
clearr93371-commits <clearr93371@gmail.com>
2026-08-06 14:25:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -819,9 +819,9 @@ def write_budget_xlsx(generated) -> bytes:
             if item['description'] in reduced_6: 
                 vat = '6%'
             elif item['description'] in no_tax_items_2: 
-                vat = '0%' if event_type == 'Pop_Up'
+                vat = '0%' if event_type == 'Pop_Up' else '23%'
             elif item['description'] in no_tax_items_1: 
-                vat = '0%' if event_type != 'Pop_Up'
+                vat = '0%' if event_type != 'Pop_Up' else '23%'
             else:
                 vat = '23%'
             ws.cell(r, 8, f"=G{r}*{vat}")
